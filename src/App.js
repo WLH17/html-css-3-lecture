c1efb450f1e30f09d8b1907e1d52734740ec4eb9
Matt Bodily <mattcbodily@gmail.com>
2020-03-04 12:29:46 -0700

    finished lecture code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      toggleDropdown: false
+    }
+  }
+
+  handleToggle = () => {
+    this.setState({toggleDropdown: !this.state.toggleDropdown})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className='main-header'>
+          <img src='https://ih1.redbubble.net/image.495915585.0747/flat,750x,075,f-pad,750x1000,f8f8f8.u2.jpg' alt='handsome squidward' className='header-logo'/>
+          {this.state.toggleDropdown
+          ? (
+            <>
+              <span className='dropdown-toggle' onClick={this.handleToggle}>X</span>
+              <nav className='mobile-links'>
+                <p>Home</p>
+                <p>About</p>
+                <p>Contact</p>
+              </nav>
+            </>
+          )
+          : (
+            <span className='dropdown-toggle' onClick={this.handleToggle}>X</span>
+          )
+          }
+          <nav className='header-links'>
+            <span>Home</span>
+            <span>About</span>
+            <span>Contact</span>
+          </nav>
+        </header>
+        <img src={logo} alt='react.js logo' className='react-logo'/>
+      </div>
+    )
+  }
 }
 
 export default App;
